b9f50342b141087d2f996e8323bc9771521a0825
brightstarmoxiz <jojololamoses@gmail.com>
2022-08-11 14:34:23 +0100

    my starting point

---
+++ video/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <h1 className="underline text-3xl">
+      what is a noun
+    </h1>
+  )
+}
+
+export default App
