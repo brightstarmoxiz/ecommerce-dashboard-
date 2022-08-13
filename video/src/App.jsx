9d23ab3e58854067da9d6431182dc6e753ba9c78
brightstarmoxiz <jojololamoses@gmail.com>
2022-08-13 06:36:05 +0100

    tailwind installation

--- video/src/App.jsx
+++ video/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-
+// import react from '@vitejs/plugin-react'
 import './App.css'
 
 const App = () => {
   return (
-    <h1 className="underline text-3xl">
-      what is a noun
-    </h1>
+  <p className="underline text-3xl uppercase">app</p>
   )
 }
 
